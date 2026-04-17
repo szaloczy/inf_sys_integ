42acaac1fa44f6897720eab1d93af76d12e7a89d
szaloczy <szaloczy6@gmail.com>
2026-04-17 21:56:14 +0200

    add tests for pressure_alert_porcessor

--- monitoring_system/tests/test_pressure_alert_processor.py
+++ monitoring_system/tests/test_pressure_alert_processor.py
@@ -1,34 +1,48 @@
-import random
 import unittest
+
 from unittest.mock import patch, MagicMock
 from pressure_alert_processor import PressureAlertProcessor
 
-@patch("pressure_alert_processor.pika.BlockingConnection")
 class TestPressureAlertProcessor(unittest.TestCase):
 
+    def setUp(self):
+        patcher = patch('pressure_alert_processor.pika.BlockingConnection')
+        self.mock_blocking_conn = patcher.start()
+        self.addCleanup(patcher.stop)
 
-    def test_pressure_is_not_high(self, mock_connection):
-        mock_connection.return_value = MagicMock()
+        mock_connection = MagicMock()
+        mock_channel = MagicMock()
+        mock_connection.channel.return_value = mock_channel
+        self.mock_blocking_conn.return_value = mock_connection
+
+
+
+    def test_is_high_pressure_func_returns_false(self):
         processor = PressureAlertProcessor()
         processor.high_pressure_counter = 1
-
         self.assertFalse(processor.is_high_pressure())
 
 
-    def test_pressure_is_high(self, mock_connection):
-        mock_connection.return_value = MagicMock()
+    def test_is_high_pressure_func_returns_true(self):
         processor = PressureAlertProcessor()
         processor.high_pressure_counter = 3
-
         self.assertTrue(processor.is_high_pressure())
 
 
     def test_counter_increase_above_8(self):
-        pass
+        processor = PressureAlertProcessor()
+
+        processor.process_measurement(8)
+        self.assertEqual(processor.high_pressure_counter, 1)
 
 
     def test_counter_resets_below_8(self):
-        pass
+        processor = PressureAlertProcessor()
+
+        processor.process_measurement(8)
+        processor.process_measurement(1)
+
+        self.assertEqual(processor.high_pressure_counter, 0)
 
 
 if __name__ == '__main__':
